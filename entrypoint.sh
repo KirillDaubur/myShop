856d344f56788f2ebef017fd2a90a1e4105068c7
KirillDaubur <daubur@gmail.com>
2020-09-14 10:05:20 +0300

    Kirill: removed unnecessary init admin functionality

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,7 @@ python3 van_oak/manage.py collectstatic --noinput
 # Apply database migrations
 echo "Apply db migrations"
 python3 van_oak/manage.py migrate
-python manage.py initadmin
-python3 van_oak/manage.py createsuperuser
+
 # Start server
 echo "Starting server"
 python van_oak/manage.py runserver 0.0.0.0:8000
